9c2be1c3d2cfc48436372da1ef0e51fc474f277d
Aaron Klotz <aklotz@mozilla.com>
2013-05-12 23:03:57 -0700

    More test_json changes

--- json/test_json.js
+++ json/test_json.js
@@ -3,95 +3,48 @@ function result(ok, msg)
   postMessage({ok: ok, msg: msg});
 }
 
-function JSONTester(iterations, users, resultFn) {
-  this.iterations = iterations;
-  this.resultFn = resultFn;
-  this.json_data = [];
-  this.obj_data = [];
-  for (var i = 0; i < users.length; ++i) {
-    var url = users[i] + ".json";
+function test(iterations)
+{
+  var json_data = [];
+  var twitter_users = ["calgaryherald", "tarasglek", "dblohm7"];
+  for (var i = 0; i < twitter_users.length; ++i) {
+    var url = twitter_users[i] + ".json";
     var request = new XMLHttpRequest();
     request.open("GET", url, false);
     request.send();
     if (request.status != 200 && request.status != 304) {
-      resultFn(false, "Failed loading " + url + ", status is " + request.status);
-    } else {
-      this.json_data.push(request.responseText);
+      result(false, "Failed loading " + url + ", status is " + request.status);
+      return false;
     }
+    json_data[i] = request.responseText;
   }
-}
+  var jsobject = [];
+  var bytes = 0;
+  var index = 0;
+  var start_ms;
+  var end_ms;
 
-function TestData(dataIndex, fn, outFn) {
-  this.dataIndex = dataIndex;
-  this.fn = fn;
-  this.results = [];
-  this.sum = 0;
-  this.outFn = outFn;
-}
-
-TestData.prototype.outputResults = function() {
-  var avg = this.sum / this.results.length;
-  var variance = 0;
-  this.results.forEach(function(x) {
-    var delta = x - avg;
-    variance += delta * delta;
-  });
-  var stddev = Math.sqrt(variance / this.results.length);
-  var stringLength = this.fn[this.fn.fieldForLength][this.dataIndex].length;
-  var cps = stringLength / (avg * 1000);
-  this.outFn(true, this.fn.name + ": String length: " + stringLength + ", Min: "
-                   + Math.min.apply(null, this.results) + " ms, Max: " 
-                   + Math.max.apply(null, this.results) + " ms, Avg: " + avg 
-                   + " ms (" + cps + " million chars per second), Std Dev: " 
-                   + stddev);
-};
-
-TestData.prototype.appendResult = function(startMs, endMs) {
-  var ms = endMs - startMs;
-  this.results.push(ms);
-  this.sum += ms;
-};
-
-JSONTester.prototype.run = function() {
-  var self = this;
-  var fns = [{name: "Parsing", fieldForLength: "input", input: self.json_data,
-              fn: JSON.parse, output: self.obj_data},
-             {name: "Serialization", fieldForLength: "output",
-              input: self.obj_data, fn: JSON.stringify, output: self.json_data}];
-  var data = [];
-  this.json_data.forEach(function(x) {
-    data.push([]);
-    fns.forEach(function(y) {
-      data[data.length - 1].push(new TestData(data.length - 1, y, self.resultFn));
-    });
-  });
-
-  for (var i = 0; i < this.json_data.length; ++i) {
-    var start_ms;
-    var end_ms;
-    for (var j = 0; j < this.iterations; ++j) {
-      for (var k = 0; k < fns.length; ++k) {
-        start_ms = Date.now();
-        fns[k].output[i] = fns[k].fn(fns[k].input[i]);
-        end_ms = Date.now();
-        data[i][k].appendResult(start_ms, end_ms);
-      }
+  for (var i = 0; i < json_data.length; ++i) {
+    start_ms = Date.now();
+    for (var j = 0; j < iterations; ++j) {
+      jsobject[i] = JSON.parse(json_data[i]);
+      bytes += json_data[i].length;
     }
-  }
-  data.forEach(function(x) {
-    x.forEach(function(y) {
-      y.outputResults();
-    });
-  });
-  this.resultFn(true, null);
-};
+    end_ms = Date.now();
+    var bytesPerSec = bytes / 1000 / (end_ms - start_ms);
+    result(true, "Parsed " + bytesPerSec + " million characters per second");
 
-function test(iterations)
-{
-  var tester = new JSONTester(iterations,
-                              ["calgaryherald", "tarasglek", "dblohm7"],
-                              result);
-  tester.run();
+    bytes = 0;
+    start_ms = Date.now();
+    for (var j = 0; j < iterations; ++j) {
+      json_data[i] = JSON.stringify(jsobject[i]);
+      bytes += json_data[i].length;
+    }
+    end_ms = Date.now();
+    bytesPerSec = bytes / 1000 / (end_ms - start_ms);
+    result(true, "Serialized " + bytesPerSec + " million characters per second");
+  }
+  result(true, null);
   return true;
 }
 
